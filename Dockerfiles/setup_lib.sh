1357b16eab282d8c02c3a305028d521c9a93562f
Kin <35365764+Kin-Zhang@users.noreply.github.com>
2022-11-30 20:10:43 +0100

    docs: update setup glog and gflag.

--- Dockerfiles/setup_lib.sh
+++ Dockerfiles/setup_lib.sh
@@ -4,10 +4,11 @@ Copyright (c) 2022 HKUST IADC
 Author: Kin (kin_eng@163.com)
 Date: 2022-06-17
 Usage: Install glog and gflags automatically
+RUN in terminal: `curl -sL https://raw.githubusercontent.com/Kin-Zhang/Kin-Zhang/main/Dockerfiles/setup_lib.sh | bash`
 '
 
-mkdir -p /workspace/lib
-cd /workspace/lib
+mkdir -p ~/workspace/tmp_lib
+cd ~/workspace/tmp_lib
 git clone https://github.com/google/glog.git
 cd glog
 git fetch --all --tags
@@ -16,9 +17,11 @@ mkdir build && cd build
 cmake .. && make -j$(nproc)
 make install
 
-cd /workspace/lib
+cd ~/workspace/tmp_lib
 git clone https://github.com/gflags/gflags.git
 cd gflags
 mkdir build && cd build
 cmake .. -DBUILD_SHARED_LIBS=ON && make
 make install
+
+rm -rf ~/workspace/tmp_lib
