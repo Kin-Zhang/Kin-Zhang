353f4f28f740d92654d1c567bceccb15ada41633
Kin <35365764+Kin-Zhang@users.noreply.github.com>
2023-03-16 17:10:11 +0100

    Update latest_glog_gflag.sh

--- Dockerfiles/latest_glog_gflag.sh
+++ Dockerfiles/latest_glog_gflag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 : '
-Copyright (c) 2022 HKUST IADC
+Copyright (c) 2022 KTH RPL
 Author: Kin (https://kin-zhang.github.io/)
 Date: 2023-03-16
 Usage: Install glog and gflags automatically
@@ -12,13 +12,13 @@ cd ~/workspace/tmp_lib
 git clone https://github.com/google/glog.git && cd glog
 mkdir build && cd build
 cmake .. && make -j$(nproc)
-make install
+make all install
 
 cd ~/workspace/tmp_lib
 git clone https://github.com/gflags/gflags.git
 cd gflags
 mkdir build && cd build
 cmake .. -DBUILD_SHARED_LIBS=ON && make
-make install
+make all install
 
 rm -rf ~/workspace/tmp_lib
